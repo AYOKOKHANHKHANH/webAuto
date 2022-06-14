a7b7f420b2646e3d7af6d9729c808e3ff58b6c24
Trần Khanh <95088817+AYOKOKHANHKHANH@users.noreply.github.com>
2022-06-14 11:25:17 +0700

    Add files via upload

---
+++ locators/createGroupLocator.py
@@ -0,0 +1,32 @@
+def get_group_button_id():
+    return "appbar-channels"
+
+def get_add_channel_button_id():
+    return "channel-add-button"
+
+def get_group_name_xpath():
+    return "/html/body/div[2]/div/div/div/div[2]/div[2]/div[2]/div[2]/input"
+
+def get_ok_button_id():
+    return "modal-button-ok"
+
+def get_close_button_id():
+    return "modal-button-cancel"
+
+def get_search_name_user_xpath():
+    return "/html/body/div[2]/div/div/div/div[2]/div[2]/div[2]/div[2]/input"
+
+def get_invite_button_xpath():
+    return "/html/body/div[2]/div/div/div/div[2]/div[2]/div[2]/div[3]/div[2]/div/div[1]/div/div/button"
+
+def get_option_button_xpath():
+    return "/html/body/div/div/div[2]/div[1]/div[2]/div[2]/div[1]/div/div/a/div[3]/div[2]/div"
+
+def get_add_friend_to_group_button_xpath():
+    return "/html/body/div/div/div[2]/div[1]/div[2]/div[2]/div[1]/div/div/a/div[3]/div[2]/div/div/div[1]/button"
+
+def get_on_off_noti_button_xpath():
+    return "/html/body/div/div/div[2]/div[1]/div[2]/div[2]/div[1]/div/div/a/div[3]/div[2]/div/div/div[2]/button"
+
+def get_del_group_button_xpath():
+    return "/html/body/div/div/div[2]/div[1]/div[2]/div[2]/div[1]/div/div/a/div[3]/div[2]/div/div/div[3]/button"
