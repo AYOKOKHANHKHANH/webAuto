ddb8c53993879ba4a03fe7fa3af76355d3237a30
Trần Khanh <95088817+AYOKOKHANHKHANH@users.noreply.github.com>
2022-07-04 09:01:39 +0700

    loginLocator.py

---
+++ locators/LoginLocator/loginLocator.py
@@ -0,0 +1,82 @@
+def get_input_phone_number_id():
+    return "phone-login-form-input-phone"
+
+
+def get_anonymous_btn_id():
+    return "platforms-halome"
+
+
+def get_anonymous_start_btn_id():
+    return "phone-login-form-button-ok"
+
+
+def get_login_continue_btn_id():
+    return "platforms-halo"
+
+
+def get_login_with_halo_btn_id():
+    return "platforms-halo"
+
+
+def get_username_id():
+    return "accountId"
+
+
+def get_pwd_id():
+    return "password"
+
+
+def get_login_btn_xpath():
+    return "/html/body/div/div[1]/div/div[1]/div/div[2]/div/div/div/div[2]/button"
+
+
+def get_otp_input_xpath():
+    return "/html/body/div/div/div[2]/div/div/div[3]/div[1]/div[1]/input"
+
+
+def get_re_otp_input_xpath():
+    return "/html/body/div/div/div[2]/div/div/div[3]/div[2]/div[1]/input"
+
+
+def get_pin_input_xpath():
+    return "/html/body/div/div/div[2]/div/div/div[3]/div/div[1]/div[1]/input"
+
+
+def get_pin_input_re_xpath():
+    return "/html/body/div/div/div[2]/div/div/div[3]/div[1]/div[2]/div[1]/input"
+
+
+def get_not_account():
+    return "/html/body/div/div/div[2]/div/div/div[3]/div/p[2]"
+
+
+def get_notify_login_fail_xpath():
+    return "/html/body/div/div[1]/div/div[1]/div/div[2]/div/div/div/div[1]/div[3]/div/div/div/div[2]/div/span"
+
+
+def get_accountId_helper_text_id():
+    return "accountId-helper-text"
+
+
+def get_password_helper_text_id():
+    return "password-helper-text"
+
+
+def get_accept_button_id():
+    return "platforms-halo"
+
+
+def get_save_button_xpath():
+    return "/html/body/div/div/div[2]/div/div/div[3]/div[2]/button/div"
+
+
+def get_resend_SMS_button_id():
+    return "phone-verify-form-button-request-sms"
+
+
+def get_enter_name_Anony_id():
+    return "phone-login-form-input-phone"
+
+
+def get_continue_button_id():
+    return "phone-login-form-button-ok"
