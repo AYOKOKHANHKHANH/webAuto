408db5c0f05f615e4d901fcd14ab425e725f7504
Trần Khanh <95088817+AYOKOKHANHKHANH@users.noreply.github.com>
2022-07-04 08:59:15 +0700

    chatlocator.py

---
+++ locators/ChatLocator/chatLocator.py
@@ -0,0 +1,38 @@
+def get_chat_button_css():
+    return ".composer.truncate.text-sm .truncate"
+
+def get_search_xpath():
+    return "/html/body/div/div/div[2]/div[1]/div[1]/input"
+
+def get_user_to_chat_button_xpath():
+    return "/html/body/div/div/div[2]/div[1]/div[2]/div[2]/div/div/a/div[2]/div"
+
+def get_input_text_chat_xpath():
+    return "/html/body/div/div/div[2]/div[2]/div/div[3]/div/div/div[2]/div[2]/div/div/p"
+
+def get_send_button_id():
+    return "composer-button-send-all"
+
+def get_send_like_button_id():
+    return "composer-button-send-like"
+
+def get_hello_start_button_xpath():
+    return "/html/body/div/div/div[2]/div[2]/div/div[3]/div"
+
+def get_emoji_chat_button_id():
+    return "/html/body/div/div/div[2]/div[2]/div/div[3]/div/div/div[4]/div"
+
+def get_emoji_button_xpath():
+    return "/html/body/div/div/div[2]/div[2]/div/div[3]/div/div/div[4]/div/div/div/div/div[2]/div/div[1]/div/button[1]"
+
+def get_text_message_receive():
+    return ".flex.group.items-center.ml-0.flex-row-reverse.mr-auto.justify-end"
+
+def get_count_new():
+    return "/html/body/div/div/div[2]/div[1]/div[2]/div[2]/div[1]/div/div/a/div[3]/div[2]/button/div"
+
+def get_receive_user_click_xpath():
+    return "/html/body/div/div/div[2]/div[1]/div[2]/div[2]/div[2]/div"
+
+def get_text_message_send():
+    return ".flex.group.items-center.ml-auto.max-w-max"
