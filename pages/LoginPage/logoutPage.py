d67d0e3fb818ff4a9dd4d911a9182d74da36a387
Trần Khanh <95088817+AYOKOKHANHKHANH@users.noreply.github.com>
2022-07-04 09:11:15 +0700

    logoutPage.py

---
+++ pages/LoginPage/logoutPage.py
@@ -0,0 +1,30 @@
+from locators.LoginLocator.logoutLocator import *
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+
+
+
+class LogoutPage:
+    def __init__(self, driver):
+        self.driver = driver
+
+    def click_avatar(self):
+        print("Click avatar")
+        avt = WebDriverWait(self.driver, 10).until(EC.element_to_be_clickable((By.XPATH, get_avt_btn_xpath())))
+        avt.click()
+
+    def click_avatar_not_img(self):
+        print("Click avatar not image")
+        avatar_not_img = WebDriverWait(self.driver, 10).until(EC.element_to_be_clickable((By.XPATH,get_avatar_not_img())))
+        avatar_not_img.click()
+
+    def click_logout(self):
+        print("Click log out")
+        logout_btn = self.driver.find_element_by_xpath(get_logout_btn_xpath())
+        logout_btn.click()
+
+    def click_ok(self):
+        print("Click ok")
+        ok_btn = self.driver.find_element_by_id(get_ok_btn_id())
+        ok_btn.click()
